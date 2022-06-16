3cd53f9b5cf5044cec01d67a45c5401bcb04acf8
akash kamat <akash.kamat.10@gmail.com>
2022-06-16 12:01:00 +0530

    minor animations

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import './Nav.css'
-export default function Nav({setRoute}) {
+export default function Nav({ setRoute }) {
   return (
-    <div onClick={()=>setRoute("home")} className='navbar'>
-        Confus...
+    <div onClick={() => setRoute("home")} className='navbar'>
+      {/* Confus... */}
+      <h1 className='nav-title'>Confus...</h1>
     </div>
   )
 }
